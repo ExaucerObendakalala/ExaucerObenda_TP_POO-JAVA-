4b763d4dfe6e304e442e1c7d3fd02d1ba01f6852
ExaucerObendakalala <exauceobendakalala@gmail.com>
2026-02-10 19:51:59 +0100

    Initial commit

---
+++ src/com/mycompany/exaucerobenda_tp_poo_java_1/ExaucerObenda_TP_POO_JAVA.java
@@ -0,0 +1,7 @@
+package com.mycompany.exaucerobenda_tp_poo_java_1;
+
+public class ExaucerObenda_TP_POO_JAVA {
+    public static void main(String... args){
+        System.out.println("Notre projet");
+    }
+}
